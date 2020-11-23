54b02751c51a739349ba47d23330fa7b62fd115a
unknown <dea8307@naver.com>
2020-11-23 13:26:40 +0900

    bakbaba

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.http import JsonResponse
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 
-from .models import Movie
+from .models import Movie, Review
+from .forms import ReviewForm
 # Create your views here.
 
 def first(request):
@@ -29,3 +31,21 @@ def movie_detail(request, movie_pk):
         'movie': movie,
     }
     return render(request, 'movies/detail.html', context)
+
+
+
+@require_http_methods(['GET', 'POST'])
+def review_create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review =  form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return  redirect('community:detail', article.pk)
+    else:
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'movies/review_create.html', context)
